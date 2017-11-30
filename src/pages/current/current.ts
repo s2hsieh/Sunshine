9ee591d1113166c57a4937a52716639a12db0ec3
Samuel Hsieh <hsieh.yunhao.samuel@gmail.com>
2017-11-30 12:06:29 -0500

    implemented PullToRefesh

--- src/pages/current/current.ts
+++ src/pages/current/current.ts
@@ -3,6 +3,7 @@ import { DataService } from './../../services/data';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { Refresher } from 'ionic-angular/components/refresher/refresher';
 
 @Component({
   templateUrl: 'current.html'
@@ -12,24 +13,28 @@ export class Current implements OnInit{
   forecast: CurrentObservation;
 
   ngOnInit(){
-    let loader = this.loadingCtrl.create({
-      content: "Plsease wait..."
-    });
-    loader.present();
-
-    this.data.getForecast("conditions").then(res => {
-      this.forecast = <CurrentObservation> res.json().current_observation;
-      console.log(this.forecast);
-      if (!this.forecast) {
-        console.log(res.json().response.error);
-      }
-      loader.dismiss();
-    }).catch(err => {
-      console.log(err);
-      loader.dismiss();
-    });
+    this.fetchData(null);
   }
 
   constructor(public navCtrl: NavController, private data: DataService, private loadingCtrl:LoadingController) {}
 
+  fetchData(refresher?:Refresher){
+    if (!refresher) {
+      var loader = this.loadingCtrl.create({
+        content: "Plsease wait..."
+      });
+      loader.present();
+    }
+    this.data.getForecast("conditions").then(res => {
+      this.forecast = <CurrentObservation> res.json().current_observation;
+      console.log(this.forecast);
+      if (!this.forecast) {
+        throw new Error("Failed to fetch data");
+      }
+      refresher ? refresher.complete() : loader.dismiss();
+    }).catch(err => {
+      console.log(err);
+      refresher ? refresher.complete() : loader.dismiss();
+    });
+  }
 }
